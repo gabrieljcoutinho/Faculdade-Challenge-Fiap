cbf3c845b7fc31f929050afe60a23f68df0fbca7
Gabriel <gabrieljcoutinho@gmail.com>
2025-05-21 16:34:35 -0300

    tirando loading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './CSS/Reset.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 
 // Components
 import Header from './components/Header';
 import Footer from './components/Footer';
-import SplashScreen from '../src/routes/pages/SplashScreen';
 
 // Pages
 import Home from './routes/pages/Home';
@@ -16,34 +14,19 @@ import Logar from './routes/pages/Logar';
 import Cadastro from './routes/pages/Cadastro';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 3000); // tempo da splash screen em milissegundos
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="App">
       <BrowserRouter>
-        {loading ? (
-          <SplashScreen />
-        ) : (
-          <>
-            <Header />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/conexoes" element={<Conexoes />} />
-              <Route path="/contato" element={<Contato />} />
-              <Route path="/configuracoes" element={<Configuracoes />} />
-              <Route path="/login" element={<Logar />} />
-              <Route path="/cadastro" element={<Cadastro />} />
-            </Routes>
-            <Footer />
-          </>
-        )}
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/conexoes" element={<Conexoes />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/configuracoes" element={<Configuracoes />} />
+          <Route path="/login" element={<Logar />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
