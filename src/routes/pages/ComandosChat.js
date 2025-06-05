5d73ecb0c0144ce3e782f559eb6b69b118e61e04
Gabriel <gabrieljcoutinho@gmail.com>
2025-06-05 13:12:30 -0300

    Fundo

--- src/routes/pages/ComandosChat.js
+++ src/routes/pages/ComandosChat.js
@@ -3,33 +3,33 @@ import '../../CSS/Comandos/comandos.css';
 
 const ComandosChat = () => {
   return (
-    <div className="comandos-container">
-      <div className='comandoChat'>
-        <h3>📢 Comandos no Chat</h3>
-      <p className="descricao">
-        Para fazer a conexão dos equipamentos via chat, utilize os seguintes comandos:
-      </p>
+    <>
+      <h3 className="comando-header">📢 Comandos no Chat</h3>
+
+      <div className="comandos-container">
+        <div className='comandoChat'>
+          <p className="descricao">
+            Para fazer a conexão dos equipamentos via chat, utilize os seguintes comandos:
+          </p>
+        </div>
+
+        <ul className="lista-comandos">
+          <li>
+            <strong>Conectar {'{nome do aparelho desejado}'}</strong><br />
+            <span className="exemplo">Ex: Conectar TV</span><br />
+            ➤ Um aparelho com o nome <strong>TV</strong> aparecerá na seção de conexões.
+          </li>
+          <br /><br /><br />
+          <li>
+            <strong>Conectar {'{ícone}'} e {'{nome do aparelho}'}</strong><br />
+            <span className="exemplo">
+              Ícones disponíveis: <code>Tv</code>, <code>arcondicionado</code>, <code>lâmpada</code>, <code>Airfry</code>, <code>Carregador</code>
+            </span><br />
+            ➤ O ícone será exibido com o nome escolhido.
+          </li>
+        </ul>
       </div>
-
-
-      <ul className="lista-comandos">
-        <li>
-          <strong>Conectar {'{nome do aparelho desejado}'}</strong><br />
-          <span className="exemplo">Ex: Conectar TV</span><br />
-          ➤ Um aparelho com o nome <strong>TV</strong> aparecerá na seção de conexões.
-        </li>
-        <br /><br /><br />
-        <li>
-          <strong>Conectar {'{ícone}'} e {'{nome do aparelho}'}</strong><br />
-          <span className="exemplo">
-            Ícones disponíveis: <code>Tv</code>, <code>arcondicionado</code>, <code>lâmpada</code>, <code>Airfry</code>, <code>Carregador</code>
-          </span><br />
-          ➤ O ícone será exibido com o nome escolhido.
-        </li>
-      </ul>
-
-
-    </div>
+    </>
   );
 };
 
