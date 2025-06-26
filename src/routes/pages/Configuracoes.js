3dbd6c42656892779db1289ff144904068a21c80
Gabriel <gabrieljcoutinho@gmail.com>
2025-06-26 08:08:59 -0300

    css que na ovai suar porem vai ficar no codigo

--- src/routes/pages/Configuracoes.js
+++ src/routes/pages/Configuracoes.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../CSS/Settings/setting.css';
 import '../../CSS/mudarTema.css'; // importe o CSS do tema aqui
-import '../../CSS/estiloBrnMudarTema.css'
+
 import comandosImg from '../../imgs/comandos.png';
 import atendimentoImg from '../../imgs/atendimento.png';
 
