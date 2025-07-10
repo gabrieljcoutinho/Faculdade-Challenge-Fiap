909d4c66df0beacb0d0d649f92d62cad8f4193bf
Gabriel <gabrieljcoutinho@gmail.com>
2025-07-10 11:07:36 -0300

    arquivo com variaveis

---
+++ src/constants/Conexao/index.js
@@ -0,0 +1,19 @@
+export const tituloPrincipal = "Aparelhos Conectados";
+export const adicionarAparelho = 'Adicionar Aparelho'
+export const nenhumAprelhoConectado = "Nenhum aparelho conectado...";
+
+export const escolherIcone = 'Escolha o ícone:'
+export const escolherCorDefundo = 'Escolha a cor de fundo:'
+export const btnBluetooth = 'Clique no botão abaixo para procurar e conectar aparelhos Bluetooth próximos:';
+export const procurarAparelhosBluetooth = 'Procurar Aparelhos Bluetooth'
+export const adicicionarAparelhoManualmente = 'Adicionar Aparelho Manualmente';
+export const esperaMenuBluetoothAbrir = 'Aguarde, a janela de seleção do navegador será aberta'
+
+export const mensagemAparelhoDesativado = 'Desativado'
+export const detalhesaparelhoAmpliados = 'Detalhes do Aparelho';
+export const mensagemExcluirAparelho = "Tem certeza que deseja remover este aparelho ?";
+
+export const tempoAparelhoConectado =  'Conectado desde:';
+export const duracaoConecxao = 'Duração da Conexão:';
+export const btnEditar = 'Editar Aparelho';
+export const btnRemover = 'Remover Aparelho';
