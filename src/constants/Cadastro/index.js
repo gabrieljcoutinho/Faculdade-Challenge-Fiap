38b09294349b3be7dc8f759a67792bd106a6103f
Gabriel <gabrieljcoutinho@gmail.com>
2025-07-10 11:07:26 -0300

    arquivo com variaveis

---
+++ src/constants/Cadastro/index.js
@@ -0,0 +1,7 @@
+export const mensagemLogar = ' Já tem uma conta?  Faça login '
+
+export const subTitulo = ' Comece sua jornada ! '
+
+export const recursos = '🚀 Acesse todos os nossos recursos.'
+export const comunidade = '🤝 Conecte-se com nossa comunidade.'
+export const suporte = '📞 Suporte dedicado para ajudar você.'
