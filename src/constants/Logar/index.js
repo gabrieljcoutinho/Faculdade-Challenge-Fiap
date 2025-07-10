1def9cb6aed30d374f143c3ef389c186963c0551
Gabriel <gabrieljcoutinho@gmail.com>
2025-07-10 11:07:52 -0300

    arquivo com variaveis

---
+++ src/constants/Logar/index.js
@@ -0,0 +1,9 @@
+export const tituloPrincipal = "Acesse sua Conta";
+export const Email = 'E-mail'
+export const Senha = 'Senha';
+export const BotaoEntrar = 'Entrar';
+export const BotaoEntrarcontaGoogle = '  Entrar com Google';
+export const BotaoEntrarContaFacebook = 'Entrar com Facebook'
+export const BotaoEntrarContaLinkedin = 'Entrar com Linkedin'
+export const esqueceuSenha = 'Esqueci minha senha';
+export const naoTemConta = 'Não tem conta? Crie uma agora';
