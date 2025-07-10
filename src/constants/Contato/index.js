1fb2bbfd6008e65d25d82b15ca4bb9e7c007f404
Gabriel <gabrieljcoutinho@gmail.com>
2025-07-10 11:07:43 -0300

    arquivo com variaveis

---
+++ src/constants/Contato/index.js
@@ -0,0 +1,15 @@
+export const tituloPrincipal ="Envie sua Mensagem"
+
+export const nome = 'Nome: '
+export const Email = 'Email: '
+export const telefone = 'Telefone: '
+export const assunto = 'Assunto: '
+export const mensagem = 'Mensagem: '
+export const anexarArquivo = 'Anexar Arquivo:'
+export const dowloadArquivo = 'Formatos permitidos: JPG, PNG, PDF, DOC'
+export const Enviarmensagem = "Enviar Mensagem"
+export const btnPerguntasFrequentes = "Ver Perguntas Frequentes"
+export const formasDecontato = "Outras Formas de Contato"
+export const rua = "Rua Exemplo, 123 - Bairro Teste, São Paulo - SP"
+export const EmailDaEmpresa = "contato@exemplo.com.br"
+export const TelefoneDaEmpresa = "(99) 99999-9999"
