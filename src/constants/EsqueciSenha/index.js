9ab68d5afecb27dc394a9e642d79487698e11c00
Gabriel <gabrieljcoutinho@gmail.com>
2025-07-10 11:07:45 -0300

    arquivo com variaveis

---
+++ src/constants/EsqueciSenha/index.js
@@ -0,0 +1,5 @@
+export const tituloPrincipal ="Esqueci minha senha"
+export const paragrafoEsqueciSenha = "Digite o e-mail abaixo e enviaremos um link para recuperar sua senha.";
+export const Email = 'Endereço de email'
+export const btnRecuperarSenha = 'Recuperar Senha'
+export const voltarPaginaLogin = 'Voltar para Login'
