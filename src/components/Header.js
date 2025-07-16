3cb858b64e3ff48a2b1e14f3c1b66edac0915eda
Gabriel <gabrieljcoutinho@gmail.com>
2025-07-16 18:48:28 -0300

    ao clciar fora do header quando tiver aberto, ele fecha

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../CSS/Header/header.css';
 import '../CSS/Header/menu.css';
@@ -6,16 +6,14 @@ import '../CSS/Header/menu.css';
 import home from '../imgs/home.png';
 import wifi from '../imgs/wifi.png';
 import contato from '../imgs/contato.png';
-import chatIcon from '../imgs/chat.png'; // Assuming you have a chat.png image
+import chatIcon from '../imgs/chat.png';
 import settingsIcon from '../imgs/engrenagem.png';
-
-
-
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const location = useLocation();
-
+  const menuRef = useRef(null);
+  const buttonRef = useRef(null);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
@@ -27,64 +25,71 @@ const Header = () => {
     document.body.classList.remove('overlay-active');
   };
 
+  // Fecha o menu se clicar fora
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        menuOpen &&
+        menuRef.current &&
+        !menuRef.current.contains(event.target) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target)
+      ) {
+        closeMenu();
+      }
+    };
 
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [menuOpen]);
 
   return (
     <header>
       <div id="main">
-        <span id="menuIcon" onClick={toggleMenu} className='btnBurguer' title='Abrir Menu'>☰</span>
+        <span
+          id="menuIcon"
+          onClick={toggleMenu}
+          className='btnBurguer'
+          ref={buttonRef}
+          title='Abrir Menu'
+        >☰</span>
       </div>
-      <div id="menu" className={menuOpen ? 'open' : ''}>
-        <button className="close-btn" onClick={closeMenu} title='Fechar Menu'> x </button>
-        <Link
-          to="/"
-          onClick={closeMenu}
-          className={`linkHeader ${location.pathname === '/' ? 'active' : ''}`}>
+
+      <div
+        id="menu"
+        className={menuOpen ? 'open' : ''}
+        ref={menuRef}
+      >
+        <button className="close-btn" onClick={closeMenu} title='Fechar Menu'>x</button>
+
+        <Link to="/" onClick={closeMenu} className={`linkHeader ${location.pathname === '/' ? 'active' : ''}`}>
           <p className='paragrafoListaHeader'><img src={home} alt="" className='iconesHeader' title='Home' /></p>
         </Link>
 
         <br />
 
-        <Link
-          to="/conexoes"
-          onClick={closeMenu}
-          className={`linkHeader ${location.pathname === '/conexoes' ? 'active' : ''}`}>
+        <Link to="/conexoes" onClick={closeMenu} className={`linkHeader ${location.pathname === '/conexoes' ? 'active' : ''}`}>
           <p className='paragrafoListaHeader'><img src={wifi} alt="" className='iconesHeader' title='Conexão' /></p>
         </Link>
 
         <br />
 
-          <Link
-            to="/chat" // You'll need to define a route for your chat page
-            onClick={closeMenu}
-            className={`linkHeader ${location.pathname === '/chat' ? 'active' : ''}`}>
-            <p className='paragrafoListaHeader'> <img src={chatIcon} alt="Chat" className='iconesHeader' title='Chat' /></p>
+        <Link to="/chat" onClick={closeMenu} className={`linkHeader ${location.pathname === '/chat' ? 'active' : ''}`}>
+          <p className='paragrafoListaHeader'><img src={chatIcon} alt="Chat" className='iconesHeader' title='Chat' /></p>
+        </Link>
+
+        <br />
+
+        <div className='contato-configuracoes'>
+          <Link to="/contato" onClick={closeMenu} className={`linkHeader ${location.pathname === '/contato' ? 'active' : ''}`}>
+            <p className='paragrafoListaHeader'><img src={contato} alt="" className='iconesHeader teste' title='Contato' /></p>
           </Link>
+
           <br />
 
-
-           <div className='contato-configuracoes'> {/* Container para contato e configurações */}
-          <Link
-            to="/contato"
-            onClick={closeMenu}
-            className={`linkHeader ${location.pathname === '/contato' ? 'active' : ''}`}>
-            <p className='paragrafoListaHeader'> <img src={contato} alt="" className='iconesHeader teste' title='Contato' /></p>
+          <Link to="/configuracoes" onClick={closeMenu} className={`linkHeader ${location.pathname === '/configuracoes' ? 'active' : ''}`}>
+            <p className='paragrafoListaHeader'><img src={settingsIcon} alt="Configurações" className='iconesHeader' title='Configurações' /></p>
           </Link>
-
-
-
-
-     <br />
-
-          <Link
-  to="/configuracoes"
-  onClick={closeMenu}
-  className={`linkHeader ${location.pathname === '/configuracoes' ? 'active' : ''}`}>
-  <p className='paragrafoListaHeader'>
-    <img src={settingsIcon} alt="Configurações" className='iconesHeader' title='Configurações' />
-  </p>
-</Link>
-
         </div>
       </div>
     </header>
