01265168cff165e00f3049987507cf0f65d0d4a6
Gabriel <gabrieljcoutinho@gmail.com>
2025-07-18 13:50:52 -0300

    espaçando conteudos

--- src/constants/EsqueciSenha/index.js
+++ src/constants/EsqueciSenha/index.js
@@ -1,5 +1,9 @@
 export const tituloPrincipal ="Esqueci minha senha"
+
 export const paragrafoEsqueciSenha = "Digite o e-mail abaixo e enviaremos um link para recuperar sua senha.";
+
 export const Email = 'Endereço de email'
+
 export const btnRecuperarSenha = 'Recuperar Senha'
+
 export const voltarPaginaLogin = 'Voltar para Login'
