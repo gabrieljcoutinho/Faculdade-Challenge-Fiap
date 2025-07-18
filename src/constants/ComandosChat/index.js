b5e7075ca6d990da9916035e282bef8bcd19f7d8
Gabriel <gabrieljcoutinho@gmail.com>
2025-07-18 13:50:44 -0300

    espaçando conteudos

--- src/constants/ComandosChat/index.js
+++ src/constants/ComandosChat/index.js
@@ -1,4 +1,7 @@
 export const tituloPrincipal = "📢 Comandos do Chat";
+
 export const subTitulo = '  Para fazer a conexão dos equipamentos via chat, utilize os seguintes comandos:'
+
 export const exemploMensagemChat = '  ➤ O ícone será exibido com o nome escolhido.'
+
 export const comandoAnlisarGrafico = ' Análise de Gráfico: Comandos '
