e078a977de6cf73bea61862e4c94e5d488c547be
Gabriel <gabrieljcoutinho@gmail.com>
2025-07-24 20:18:59 -0300

    tirando js

--- src/routes/pages/Conexoes.js
+++ src/routes/pages/Conexoes.js
@@ -564,6 +564,7 @@ const Conexoes = ({ conexions, setConexions, onConnectDevice, onRemoveDevice, on
 </div>
 
 
+
       {selectedConexion && (
         <div className="modal-overlay" onClick={() => setSelectedConexion(null)}>
           <div className="detalhes-aparelho-modal" onClick={e => e.stopPropagation()}>
