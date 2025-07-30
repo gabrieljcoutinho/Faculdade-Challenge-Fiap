118fd2819fbc41317871d5064be5f54e2336501a
Gabriel <gabrieljcoutinho@gmail.com>
2025-07-30 20:08:16 -0300

    conteudo erro 404

--- src/routes/pages/NotFound.js
+++ src/routes/pages/NotFound.js
@@ -1,13 +1,14 @@
 // src/routes/pages/NotFound.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../../CSS/NotFound/index.css'; // Importe o novo arquivo CSS
 
 const NotFound = () => {
   return (
-    <div style={{ textAlign: 'center', padding: '100px 20px' }}>
-      <h1 style={{ fontSize: '4rem', color: '#ff5555' }}>404</h1>
-      <p style={{ fontSize: '1.5rem' }}>Página não encontrada.</p>
-      <Link to="/" style={{ color: '#0FF0FC', textDecoration: 'underline' }}>
+    <div className="notfound-container">
+      <h1 className="notfound-title">404</h1>
+      <p className="notfound-text">Página não encontrada.</p>
+      <Link to="/" className="notfound-link">
         Voltar para a página inicial
       </Link>
     </div>
