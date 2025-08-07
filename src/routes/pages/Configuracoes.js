793c6b84a87d5d09e3963030a62b955b7a7f1ce2
Gabriel <gabrieljcoutinho@gmail.com>
2025-08-07 09:33:50 -0300

    colcoand oa rota do novo componente

--- src/routes/pages/Configuracoes.js
+++ src/routes/pages/Configuracoes.js
@@ -13,6 +13,7 @@ import { logar, deslogar } from '../../constants/Configuracao/index.js';
 
 import comandosImg from '../../imgs/imgConfiguracao/comandos.png';
 import atendimentoImg from '../../imgs/imgConfiguracao/atendimento.png';
+import relogioImg from '../../imgs/imgConfiguracao/relogio.png';
 
 const Configuracoes = ({ isReading, toggleReading }) => {
   const navigate = useNavigate();
@@ -50,6 +51,10 @@ const Configuracoes = ({ isReading, toggleReading }) => {
 
           <button className="comando-btn" title="Central de Ajuda" onClick={() => navigate('/helpCenter')}>
             <img src={atendimentoImg} alt="Atendimento" className="imgComando" />
+          </button>
+
+          <button className="comando-btn" onClick={() => navigate('/timer')}>
+            <img src={relogioImg} alt="Marcar conexão" className="imgComando" />
           </button>
 
           <button
