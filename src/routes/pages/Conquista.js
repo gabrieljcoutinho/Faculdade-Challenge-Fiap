c5204f8cfc4d24cd248ef2930c53e666bbe288c9
Gabriel <gabrieljcoutinho@gmail.com>
2025-08-25 10:27:40 -0300

    conquyista aentrar na pagian ativada

--- src/routes/pages/Conquista.js
+++ src/routes/pages/Conquista.js
@@ -1,39 +1,46 @@
-import React from 'react'
-import '../../CSS/Conquista/index.css'
+import React, { useEffect, useState } from 'react';
+import '../../CSS/Conquista/index.css';
 
-import imgEntrada from '../../imgs/Medalhas/1-entrouNaPáginaPelaPrimeraVez.png'
-import imgGrafico from '../../imgs/Medalhas/2-grafico.png'
-import secoes from '../../imgs/Medalhas/3-acesouTodasAsSecoesDaPaginaPeloMenosUmaVez.png'
-import relatorio from '../../imgs/Medalhas/4-visualizouoRelatorioDiarioDoGRafico.png'
-import conexao from '../../imgs/Medalhas/5-conectou UMaprelhoPelaPrimeriaVez.png'
-import excluiu from '../../imgs/Medalhas/6-excluiuUmAparelhoPelaPrimeraVez.png'
-import compartilhar from '../../imgs/Medalhas/7-acessouOqrcodePElaPriemraVez.png'
-import chat from '../../imgs/Medalhas/8-acessouOchatPelaPriemraVez.png'
-import goodwe from '../../imgs/Medalhas/9-clicouNoLogoDaEmpresa.png'
-import tema from '../../imgs/Medalhas/10-mudouOtemDaPagina.png'
-import acessibilidade from '../../imgs/Medalhas/11-acessibilidade.png'
-import tudo from '../../imgs/Medalhas/12-pegouTodasAsConquistas.png'
+import imgEntrada from '../../imgs/Medalhas/1-entrouNaPáginaPelaPrimeraVez.png';
+import imgGrafico from '../../imgs/Medalhas/2-grafico.png';
+import secoes from '../../imgs/Medalhas/3-acesouTodasAsSecoesDaPaginaPeloMenosUmaVez.png';
+import relatorio from '../../imgs/Medalhas/4-visualizouoRelatorioDiarioDoGRafico.png';
+import conexao from '../../imgs/Medalhas/5-conectou UMaprelhoPelaPrimeriaVez.png';
+import excluiu from '../../imgs/Medalhas/6-excluiuUmAparelhoPelaPrimeraVez.png';
+import compartilhar from '../../imgs/Medalhas/7-acessouOqrcodePElaPriemraVez.png';
+import chat from '../../imgs/Medalhas/8-acessouOchatPelaPriemraVez.png';
+import goodwe from '../../imgs/Medalhas/9-clicouNoLogoDaEmpresa.png';
+import tema from '../../imgs/Medalhas/10-mudouOtemDaPagina.png';
+import acessibilidade from '../../imgs/Medalhas/11-acessibilidade.png';
+import tudo from '../../imgs/Medalhas/12-pegouTodasAsConquistas.png';
 
 const Conquista = () => {
+  const [primeiraVisita, setPrimeiraVisita] = useState(false);
+
+  useEffect(() => {
+    // Ao entrar na página, marca a primeira visita como desbloqueada
+    setPrimeiraVisita(true);
+  }, []);
+
   return (
     <div className='conquista'>
       <h1>Conquistas</h1>
       <div className="conquistas-container">
-        <img src={imgEntrada} alt="" />
-        <img src={imgGrafico} alt="" />
-        <img src={secoes} alt="" />
-        <img src={relatorio} alt="" />
-        <img src={conexao} alt="" />
-        <img src={excluiu} alt="" />
-        <img src={compartilhar} alt="" />
-        <img src={chat} alt="" />
-        <img src={goodwe} alt="" />
-        <img src={tema} alt="" />
-        <img src={acessibilidade} alt="" />
-        <img src={tudo} alt="" />
+        <img src={imgEntrada} alt="Primeira visita" className={primeiraVisita ? 'ativa' : ''} />
+        <img src={imgGrafico} alt="Gráfico" />
+        <img src={secoes} alt="Seções" />
+        <img src={relatorio} alt="Relatório" />
+        <img src={conexao} alt="Conexão" />
+        <img src={excluiu} alt="Excluiu aparelho" />
+        <img src={compartilhar} alt="Compartilhar" />
+        <img src={chat} alt="Chat" />
+        <img src={goodwe} alt="GoodWe" />
+        <img src={tema} alt="Tema" />
+        <img src={acessibilidade} alt="Acessibilidade" />
+        <img src={tudo} alt="Todas conquistas" />
       </div>
     </div>
   )
 }
 
-export default Conquista
+export default Conquista;
