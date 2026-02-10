8f80024cc7960fbb6a133b6cb4edaf37cb6b5742
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-10 20:24:30 -0300

    renomeando

--- src/routes/pages/EsqueciSenha.js
+++ src/routes/pages/EsqueciSenha.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import '../../CSS/EsqueciSenha/resetJs.module.css';
 import '../../CSS/EsqueciSenha/layout.module.css';
-import '../../CSS/EsqueciSenha/form.css';
+import '../../CSS/EsqueciSenha/form.module.css';
 import '../../CSS/EsqueciSenha/button.css';
 import '../../CSS/EsqueciSenha/animations.module.css';
 import '../../CSS/EsqueciSenha/messagens.css';
