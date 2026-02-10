30a3b050f27a2905a34fa5b030f0733086f4b708
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-10 20:35:00 -0300

    renomeando

--- src/routes/pages/Cadastro.js
+++ src/routes/pages/Cadastro.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../../CSS/Cadastro/cadastro.module.css';
-import '../../CSS/Cadastro/input.css';
+import '../../CSS/Cadastro/input.module.css';
 import '../../CSS/Cadastro/password.module.css';
 import '../../CSS/Cadastro/links.module.css';
 import '../../CSS/Cadastro/error.module.css';
