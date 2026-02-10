f539a337c1bcb7a5b4241ad0e535c6b1db6dc8a7
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-10 20:39:21 -0300

    renomeando

--- src/routes/pages/Logar.js
+++ src/routes/pages/Logar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import '../../CSS/Login/container.css';
+import '../../CSS/Login/container.module.css';
 import '../../CSS/Login/login-box.css';
 import '../../CSS/Login/social-buttons.css';
 import '../../CSS/Login/separator.css';
