29450a00b3c4d623e46a529827216d98349df95e
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-10 20:29:08 -0300

    renomeando

--- src/routes/pages/Configuracoes.js
+++ src/routes/pages/Configuracoes.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 
 import '../../CSS/Settings/overlay.module.css';
 import '../../CSS/Settings/buttons.module.css';
-import '../../CSS/Settings/layout.css';
+import '../../CSS/Settings/layout.module.css';
 import '../../CSS/Settings/images.css';
 import '../../CSS/Settings/confirmarDeslogar.css';
 
