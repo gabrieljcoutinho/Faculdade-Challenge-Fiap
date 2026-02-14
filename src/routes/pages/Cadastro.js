96351c20909da4bf0c92a2fbfe1b58e76b09bf07
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-14 10:40:28 -0300

    novo css

--- src/routes/pages/Cadastro.js
+++ src/routes/pages/Cadastro.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import '../../CSS/Cadastro/cadastro.module.css';
 import '../../CSS/Cadastro/input.module.css';
-import '../../CSS/Cadastro/password.css';
+import '../../CSS/Cadastro/password.module.css';
 import '../../CSS/Cadastro/links.css';
 import '../../CSS/Cadastro/error.css';
 import { mensagemLogar, subTitulo, recursos, comunidade, suporte, titulo} from '../../constants/Cadastro/index.js';
