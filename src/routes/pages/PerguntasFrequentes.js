de82766c792094e98d91fb6ba2ed72a6fe55af70
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-14 10:58:44 -0300

    renomenado arquivo

--- src/routes/pages/PerguntasFrequentes.js
+++ src/routes/pages/PerguntasFrequentes.js
@@ -1,6 +1,6 @@
 // src/routes/pages/PerguntasFrequentes.js
 import React, { useState } from 'react';
-import '../../CSS/PerguntasFrequentes/perguntasFrequentes.css';
+import '../../CSS/PerguntasFrequentes/perguntasFrequentes.module.css';
 import '../../CSS/PerguntasFrequentes/btnPerguntasFrequentes.css';
 
 const PerguntasFrequentes = ({ isReading }) => {
