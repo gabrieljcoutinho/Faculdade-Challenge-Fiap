448ed93a5180f22ffd6908a7f34bb1c880a67e2e
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-14 10:41:27 -0300

    novo css

--- src/routes/pages/Cadastro.js
+++ src/routes/pages/Cadastro.js
@@ -4,7 +4,7 @@ import '../../CSS/Cadastro/cadastro.module.css';
 import '../../CSS/Cadastro/input.module.css';
 import '../../CSS/Cadastro/password.module.css';
 import '../../CSS/Cadastro/links.module.css';
-import '../../CSS/Cadastro/error.css';
+import '../../CSS/Cadastro/error.module.css';
 import { mensagemLogar, subTitulo, recursos, comunidade, suporte, titulo} from '../../constants/Cadastro/index.js';
 
 const Cadastro = () => {
